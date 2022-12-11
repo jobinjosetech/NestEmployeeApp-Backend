94aa00f0c40a04f8344a8e9b90916a8dd59054a3
jobin <jobinjose.in@gmail.com>
2022-12-11 12:02:41 +0530

    View Task

--- src/main/java/com/nest/NestEmployeeApp/controller/TaskController.java
+++ src/main/java/com/nest/NestEmployeeApp/controller/TaskController.java
@@ -1,30 +1,40 @@
 package com.nest.NestEmployeeApp.controller;
 
 import com.nest.NestEmployeeApp.dao.TaskDao;
-import com.nest.NestEmployeeApp.models.Employee;
 import com.nest.NestEmployeeApp.models.Task;
+import org.hibernate.annotations.Any;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class TaskController {
-
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     @Autowired
     private TaskDao tdao;
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/addTask", consumes = "application/json", produces = "application/json")
     public HashMap<String, String> AddEmployee(@RequestBody Task task){
+        LocalDateTime now = LocalDateTime.now();
         task.setTaskStatus(false);
+        task.setTaskDate(dtf.format(now));
+        task.setTaskCompleteDate("Nil");
         tdao.save(task);
         HashMap<String, String> status = new HashMap<>();
         status.put("status","success");
         return status;
+    }
+
+    @GetMapping("/viewStatusTask")
+    public List<Map<String, String>> ViewStatusTask(){
+        System.out.println();
+        return tdao.GetTask();
     }
 
 }
--- src/main/java/com/nest/NestEmployeeApp/models/Task.java
+++ src/main/java/com/nest/NestEmployeeApp/models/Task.java
@@ -14,16 +14,20 @@ public class Task {
     private String taskTitle;
     private String taskDescription;
     private boolean taskStatus;
+    private String taskDate;
+    private String taskCompleteDate;
     private int empId;
 
     public Task() {
     }
 
-    public Task(int id, String taskTitle, String taskDescription, boolean taskStatus, int empId) {
+    public Task(int id, String taskTitle, String taskDescription, boolean taskStatus, String taskDate, String taskCompleteDate, int empId) {
         this.id = id;
         this.taskTitle = taskTitle;
         this.taskDescription = taskDescription;
         this.taskStatus = taskStatus;
+        this.taskDate = taskDate;
+        this.taskCompleteDate = taskCompleteDate;
         this.empId = empId;
     }
 
@@ -57,6 +61,22 @@ public class Task {
 
     public void setTaskStatus(boolean taskStatus) {
         this.taskStatus = taskStatus;
+    }
+
+    public String getTaskDate() {
+        return taskDate;
+    }
+
+    public void setTaskDate(String taskDate) {
+        this.taskDate = taskDate;
+    }
+
+    public String getTaskCompleteDate() {
+        return taskCompleteDate;
+    }
+
+    public void setTaskCompleteDate(String taskCompleteDate) {
+        this.taskCompleteDate = taskCompleteDate;
     }
 
     public int getEmpId() {
