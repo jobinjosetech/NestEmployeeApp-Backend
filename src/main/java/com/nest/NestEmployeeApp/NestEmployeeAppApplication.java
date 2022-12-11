363425233333295734ba8c67b254aa1572740576
jobin <jobinjose.in@gmail.com>
2022-12-11 09:28:34 +0530

    NEst Employee App

---
+++ src/main/java/com/nest/NestEmployeeApp/NestEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.nest.NestEmployeeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployeeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/NestEmployeeApp/NestEmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.NestEmployeeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestEmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
